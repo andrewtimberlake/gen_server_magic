c2fcc4274f6a86f4c69373fc374c961a5802c4b9
Andrew Timberlake <andrew@andrewtimberlake.com>
2018-05-04 13:56:15 +0200

    Release 0.0.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GenServerMagic.MixProject do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "0.0.2"
   @github_url "https://github.com/andrewtimberlake/gen_server_magic"
 
   def project do
